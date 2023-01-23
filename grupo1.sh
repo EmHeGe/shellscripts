083f8d5936d2c3e5974f58c53390f928f03679cc
Emilio Herrera <emhege@floridauniversitaria.es>
2023-01-23 10:13:59 +0100

    grupo 1 realizado

--- grupo1.sh
+++ grupo1.sh
@@ -1,1 +1,5 @@
-test
+#! /bin/bash
+read -p "Introduzca su nombre:" nombre
+read -p "Introduzca su primer apellido:" apellUno
+read -p "Introduzca su segundo apellido:" apellDos
+echo "Tu nombre completo es " $nombre $apellUno $apellDos
